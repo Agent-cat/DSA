2b5cb8d1dcf52985091c7f6c6d7a9cf5dc277a81
Agent-cat <2300031042@kluniversity.in>
2025-07-03 22:35:52 +0530

    30 lpa

---
+++ Dsajava/one.java
@@ -0,0 +1,14 @@
+package Dsajava;
+
+public class one {
+    public static void main(String[] args) {
+         String s="1/2+5/2+6/3";
+    String[] a=s.split("\\+");
+        double x=Integer.parseInt(a[0]);
+        System.out.println(x);  
+    
+    }
+   
+    
+    
+}
