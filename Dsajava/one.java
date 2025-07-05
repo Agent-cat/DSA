be515aed86aba7c0e2d71db9208b5ac28f34a5cc
Agent-cat <2300031042@kluniversity.in>
2025-07-05 23:08:13 +0530

    one.java

--- Dsajava/one.java
+++ Dsajava/one.java
@@ -1,14 +1,46 @@
 package Dsajava;
 
-public class one {
+import java.util.Scanner;
+
+class one {
     public static void main(String[] args) {
-         String s="1/2+5/2+6/3";
-    String[] a=s.split("\\+");
-        double x=Integer.parseInt(a[0]);
-        System.out.println(x);  
-    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter tthr rows and coluns of matrix A");
+        int rowsA = sc.nextInt();
+        int colsA = sc.nextInt();
+        System.out.println("Enter tthr rows and coluns of matrix B");
+        int rowsB = sc.nextInt();
+        int colsB = sc.nextInt();
+
+        int[][] matA = new int[rowsA][colsA];
+        int[][] matB = new int[rowsB][colsB];
+        int[][] result = new int[rowsA][colsB];
+
+        if (colsA != rowsB) {
+            return;
+        }
+        for (int i = 0; i < rowsA; i++)
+            for (int j = 0; j < colsA; j++) {
+                matA[i][j] = sc.nextInt();
+            }
+        for (int i = 0; i < rowsB; i++)
+            for (int j = 0; j < colsB; j++) {
+                matB[i][j] = sc.nextInt();
+            }
+
+        for (int i = 0; i < rowsA; i++)
+            for (int j = 0; j < colsB; j++)
+                for (int k = 0; k < colsA; k++)
+                    result[i][j] += matA[i][k] * matB[k][j];
+
+        System.out.println("Result of Matrix Multiplication:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
-   
-    
-    
+
 }
